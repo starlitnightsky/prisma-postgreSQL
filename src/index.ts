2e567f745fcbab5768c932034e59b24591910db1
starlitnightsky <miraclehourglass@gmail.com>
2022-12-03 02:39:24 +0800

    CRUD user

--- src/index.ts
+++ src/index.ts
@@ -10,32 +10,62 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-app.post("/users", async (req, res) => {
-  const { email, name } = req.body;
+app
+  .route("/users")
+  .post(async (req, res) => {
+    const { email, name } = req.body;
 
-  if (!email) {
-    return res.status(400).json({
-      status: false,
-      message: "email is required",
+    if (!email) {
+      return res.status(400).json({
+        status: false,
+        message: "email is required",
+      });
+    }
+
+    const userWithEmail = await prisma.user.findUnique({ where: { email } });
+    if (userWithEmail) {
+      return res.status(400).json({
+        status: false,
+        message: "User already exists with email",
+      });
+    }
+
+    const created = await prisma.user.create({
+      data: { email, name },
     });
-  }
 
-  const userWithEmail = await prisma.user.findUnique({ where: { email } });
-  if (userWithEmail) {
-    return res.status(400).json({
-      status: false,
-      message: "User already exists with email",
-    });
-  }
-
-  const created = await prisma.user.create({
-    data: { email, name },
+    return res.status(200).json(created);
+  })
+  .get(async (req, res) => {
+    const users = await prisma.user.findMany();
+    res.status(200).json(users);
   });
 
-  console.log("[user.created]", created);
+app
+  .route("/users/:id")
+  .get(async (req, res) => {
+    const userId = parseInt(req.params.id, 10);
+    return prisma.user
+      .findUnique({ where: { id: userId } })
+      .then((user: any) => res.status(200).json(user));
+  })
+  .patch(async (req, res) => {
+    const userId = parseInt(req.params.id, 10);
+    const user = await prisma.user.findUnique({ where: { id: userId } });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ status: false, message: "Not found the user!" });
+    }
 
-  return res.status(200).json(created);
-});
+    const { email, name } = req.body;
+    const updatedUser = await prisma.user.update({
+      where: { id: userId },
+      data: { email, name },
+    });
+
+    return res.status(200).json(updatedUser);
+  });
 
 app.listen(3000, () => {
   console.log("🚀 Sever ready at: http://localhost:3000");
