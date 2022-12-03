efb7328cc9eca6be364f21874c1e71bdab098209
starlitnightsky <miraclehourglass@gmail.com>
2022-12-04 00:53:35 +0800

    API to add a post

--- src/routes/posts.route.ts
+++ src/routes/posts.route.ts
@@ -1,22 +1,35 @@
 import { Router } from "express";
 import passport from "passport";
+import type { User } from ".prisma/client";
 import prisma from "../config/prisma";
-// import * as passport from "../config/passport";
 
 const router = Router();
 
-router.use((req, res, next) => {
-  console.log("passport.middleware.before");
-  next();
-});
-
 router.use(passport.authenticate("jwt", { session: false }));
 
-router.post("/s", (req, res) => {
-  console.log("did you call me?");
-  return res
-    .status(200)
-    .json({ status: true, message: "you called [POST]posts/." });
+router.post("/", (req, res) => {
+  const { title, content } = req.body;
+  const published = !!req.body.published;
+  const authorId = (req.user as User).id;
+
+  return Promise.resolve()
+    .then(async () => {
+      if (!title) throw new Error("Title is required!");
+
+      const post = await prisma.post.create({
+        data: {
+          title,
+          content,
+          authorId,
+          published,
+        },
+      });
+
+      return res.status(200).json(post);
+    })
+    .catch((err) => {
+      return res.status(400).json({ status: false, message: err.message });
+    });
 });
 
 export default router;
