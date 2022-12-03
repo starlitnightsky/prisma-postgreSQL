b8f168467b6f0f15bfd513863dc1e71a402d13df
starlitnightsky <miraclehourglass@gmail.com>
2022-12-04 00:53:17 +0800

    updated auth with passport-jwt

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -1,4 +1,3 @@
-import express from "express";
 import type { Express } from "express";
 import passport from "passport";
 import passportLocal from "passport-local";
@@ -7,7 +6,6 @@ import { Request, Response, NextFunction } from "express";
 import prisma from "./prisma";
 import { appConfig } from "./app";
 import { comparePassword } from "../utils/password";
-console.log("[Passport][Config]");
 
 const LocalStrategy = passportLocal.Strategy;
 const JWTStrategy = passportJWT.Strategy;
@@ -16,32 +14,12 @@ const ExtractJWT = passportJWT.ExtractJwt;
 export const configure = (app: Express) => {
   app.use(passport.initialize());
   app.use(passport.session());
-  passport.serializeUser<any, any>((req, user, done) => {
-    console.log("[passport.serializeUser]", user);
-    done(undefined, user);
-  });
-
-  passport.deserializeUser((id, done) => {
-    console.log("[passport.deserializeUser]", id);
-    // User.findById(id, (err: NativeError, user: UserDocument) => done(err, user));
-    return prisma.user
-      .findUnique({ where: { id: id as number } })
-      .then((user) => {
-        if (user) {
-          return done(undefined, user);
-        }
-        return done(new Error("Not found the user!"), false);
-      })
-      .catch((err) => {
-        return done(err, false);
-      });
-  });
 
   passport.use(
+    "login",
     new LocalStrategy(
       { usernameField: "email", passwordField: "password" },
       (email, password, done) => {
-        console.log("passport.local", email, password);
         return prisma.user
           .findUnique({ where: { email } })
           .then(async (user) => {
@@ -50,8 +28,7 @@ export const configure = (app: Express) => {
                 message: `Email ${email} not found.`,
               });
 
-            // compare password.
-            const isMatch = await comparePassword(password, user.password); // TODO: some func to compare password.
+            const isMatch = await comparePassword(password, user.password);
             if (isMatch) return done(undefined, user);
             return done(undefined, false, {
               message: "Invalid email or password",
@@ -66,12 +43,10 @@ export const configure = (app: Express) => {
     new JWTStrategy(
       {
         jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-        // jwtFromRequest: ExtractJWT.fromAuthHeaderWithScheme("jwt"),
         secretOrKey: appConfig.JWT_SECRET,
       },
       (jwtPayload, cb) => {
         console.log("jwtstrategy.callback", jwtPayload);
-        return cb(null, jwtPayload);
         return prisma.user
           .findUnique({ where: { id: jwtPayload.id } })
           .then((user) => {
@@ -84,16 +59,4 @@ export const configure = (app: Express) => {
       }
     )
   );
-};
-
-export const isAuthenticated = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  if (req.isAuthenticated()) {
-    next();
-  } else {
-    throw new Error("Authenticaion failed");
-  }
 };
--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,17 +1,12 @@
 import { Router } from "express";
-import bcrypt from "bcrypt";
 import passport from "passport";
+import type { User } from ".prisma/client";
 import jwt from "jsonwebtoken";
 import prisma from "../config/prisma";
 import { appConfig } from "../config/app";
-import { comparePassword, encryptPassword } from "../utils/password";
+import { encryptPassword } from "../utils/password";
 
 const router = Router();
-
-router.use((req, res, next) => {
-  console.log("auth.router.middleware");
-  next();
-});
 
 router.post("/register", async (req, res) => {
   const { email, name, password } = req.body;
@@ -26,21 +21,19 @@ router.post("/register", async (req, res) => {
   return Promise.resolve()
     .then(async () => {
       const hashedPassword = await encryptPassword(password);
-
       const emailUsed = await prisma.user.findUnique({ where: { email } });
+
       if (emailUsed) {
         throw new Error("User already exists with email");
       }
-      console.log("register.email.available", email);
 
       const user = await prisma.user.create({
         data: { email, name, password: hashedPassword },
       });
-      console.log("user.created?", user);
-      return {};
+
+      return user;
     })
     .then((user) => {
-      // console.log("[POST]/register.user.created", user);
       return res.json(user);
     })
     .catch((err) => {
@@ -58,30 +51,24 @@ router.post("/login", async (req, res, next) => {
     if (!email || !password) {
       reject(new Error("email, password are required."));
     }
-    passport.authenticate("local", { session: false }, (err, user, info) => {
+    passport.authenticate("login", { session: false }, (err, user, info) => {
       if (err) reject(err);
       if (info?.message) reject(new Error(info.message));
-
-      req.login(user, { session: false }, (err) => {
-        if (err) {
-          reject(err);
-        }
-        // generate a signed son web token with the contents of user object and return it in the response
-        // const token = jwt.sign(user, "your_jwt_secret");
-        // return res.json({ user, token });
-        resolve(user);
-      });
-
-      // console.log(req.login);
-      // resolve(user);
+      resolve(user);
     })(req, res, next);
   })
     .then((user) => {
-      console.log("login.response", user);
-      const token = jwt.sign(user!, appConfig.JWT_SECRET, {
-        expiresIn: "7d",
-      });
-      return res.status(200).json({ user, token });
+      const { id } = user as User;
+      const token = jwt.sign(
+        {
+          id,
+        },
+        appConfig.JWT_SECRET,
+        {
+          expiresIn: "7d",
+        }
+      );
+      return res.status(200).json({ status: true, token });
     })
     .catch((err) => {
       return res.status(400).json({
